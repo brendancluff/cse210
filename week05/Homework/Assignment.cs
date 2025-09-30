8f6a12ec9767acf9a12681223b7d686641c4a4e2
brendancluff <cluffbrendan04@gmail.com>
2025-09-30 14:58:55 -0700

    W05 Learning Activity: Inheritance

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,24 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    // Constructor
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    // Method to get summary
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Protected getter for derived classes
+    protected string GetStudentName()
+    {
+        return _studentName;
+    }
+}
